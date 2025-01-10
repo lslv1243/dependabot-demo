55e5c3542486d555551772543fa37578d933afec
Leonardo da Silva <lslv1243@gmail.com>
2025-01-10 10:22:36 -0300

    introduce package with known vulnerability

--- swift/Package.swift
+++ swift/Package.swift
@@ -5,10 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "swift",
+    platforms: [
+        .macOS(.v10_15)
+    ],
+    dependencies: [
+        // Adding Vapor as a dependency with version 4.61.0
+        .package(url: "https://github.com/vapor/vapor.git", exact: "4.61.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "swift"),
+            name: "swift",
+            dependencies: [
+                // Adding Vapor as a dependency to the target
+                .product(name: "Vapor", package: "vapor")
+            ]
+        ),
     ]
 )
